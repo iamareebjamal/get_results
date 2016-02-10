2de4d97784049804ccb3281b6ac209db6396c714
iamareebjamal <jamal.areeb@gmail.com>
2016-02-10 23:07:35 +0530

    New result format

--- utils.py
+++ utils.py
@@ -44,8 +44,8 @@ def load_all(students):
       print('HTML file not found. Download files by option 1 first...') 
       return
     try:
-      spi = soup('table')[2].findAll('tr')[1].findAll('th')[4].string.strip()
-      cpi = soup('table')[2].findAll('tr')[1].findAll('th')[5].string.strip()
+      spi = soup('table')[1].findAll('td')[4].string.strip()
+      cpi = soup('table')[1].findAll('td')[5].string.strip()
     except IndexError as err:
       spi = 0
       cpi = 0
