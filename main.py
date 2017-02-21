1547c693d7cd333fdde6e04a3f00170e589731a5
iamareebjamal <jamal.areeb@gmail.com>
2017-02-21 23:10:42 +0530

    style: Fix indentation

--- main.py
+++ main.py
@@ -1,142 +1,141 @@
 import utils
-utils.mkdirs()
-#Let's create all needed directories before anything else!
 
-import requests 
+utils.mkdirs()
+# Let's create all needed directories before anything else!
+
+import requests
 import os
 import enlist
 from bs4 import BeautifulSoup
 
-
-
 welcome = \
-"""
+    """
 
-AMU B.Tech Results Downloader
-This Python script downloads B.Tech Results of whole class based on information in attendance Excel file.
+    AMU B.Tech Results Downloader
+    This Python script downloads B.Tech Results of whole class based on information in attendance Excel file.
 
-First you need to put Excel file in Input/ folder
-Then type the name of file when asked (Default : store.xlsx)
-This will load the information about students from the Excel file and stores it in students.db for future faster access.
-Then you will prompted 3 options:
+    First you need to put Excel file in Input/ folder
+    Then type the name of file when asked (Default : store.xlsx)
+    This will load the information about students from the Excel file and stores it in students.db for future faster access.
+    Then you will prompted 3 options:
 
-    First option downloads the result of whole class and
-    stores them as html pages in Store/ folder. 
-    Note : This option should be run at least once to
-    download  all necessary result files for further options
-    If there are no result files in Store/ folder, then
-    script will not run properly.
-   
-    Second option loads CPI and SPI from downloaded
-    html to script database as results.db for future faster
-    access.
-    Note : This option is necessary to be run in
-    order to run 3rd option. If this option is not run,
-    then no data can be written in Excel file. 
-    
-    Third option reads your CPI and SPI from Updated
-    database and saves the information as Excel file in
-    Output/ folder.
-    
-    
-Note: App creates required files and databases in iaj/ folder for proper functioning. Please don't delete those files. 
-    
-    
-Let's start:
+        First option downloads the result of whole class and
+        stores them as html pages in Store/ folder.
+        Note : This option should be run at least once to
+        download  all necessary result files for further options
+        If there are no result files in Store/ folder, then
+        script will not run properly.
 
-"""
+        Second option loads CPI and SPI from downloaded
+        html to script database as results.db for future faster
+        access.
+        Note : This option is necessary to be run in
+        order to run 3rd option. If this option is not run,
+        then no data can be written in Excel file.
+
+        Third option reads your CPI and SPI from Updated
+        database and saves the information as Excel file in
+        Output/ folder.
+
+
+    Note: App creates required files and databases in iaj/ folder for proper functioning. Please don't delete those files.
+
+
+    Let's start:
+
+    """
 
 os.chdir(utils.path())
 
 url = 'http://ctengg.amu.ac.in/result_btech.php'
-    
+
 # Let's Go!
 
+print(welcome)
 
-
-print(welcome) 
 
 def for_student(fac_no, en_no, name):
-  r_no = fac_no[5:8]
-  path = os.path.join(utils.path(), 'iaj', 'Store')
-  file_name = r_no + ' - ' + name + ' (' + fac_no + ')' +'.html'
-  os.chdir(path)
-  if os.path.isfile(file_name):
-    print('File Exists... Skipping\n\t', file_name)
-  else :
-    try:
-      form_data = {'FN':fac_no, 'EN':en_no, 'submit':'submit'}
-      response = requests.post(url, data=form_data)
-      
-      soup = BeautifulSoup(response.text)
-      with open(file_name , 'w+') as ou:
-        print(soup.prettify(), file=ou)
-        
-      
-      if 'CPI' in response.text :
-        print('Saved result of', name)
-      elif 'This Result has not been declared yet!' in response.text:
-        print('No result')
-      elif 'Faculty_No or En_No is incorrect!' in response.text :
-        print('Wrong Faculty or Enrolment No.')
-      else:
-        print('Wrong input data or no result...')
-    
-    except requests.exceptions.ConnectionError:
-      print('No Connection')
-  
+    r_no = fac_no[5:8]
+    path = os.path.join(utils.path(), 'iaj', 'Store')
+    file_name = r_no + ' - ' + name + ' (' + fac_no + ')' + '.html'
+    os.chdir(path)
+    if os.path.isfile(file_name):
+        print('File Exists... Skipping\n\t', file_name)
+    else:
+        try:
+            form_data = {'FN': fac_no, 'EN': en_no, 'submit': 'submit'}
+            response = requests.post(url, data=form_data)
+
+            soup = BeautifulSoup(response.text)
+            with open(file_name, 'w+') as ou:
+                print(soup.prettify(), file=ou)
+
+            if 'CPI' in response.text:
+                print('Saved result of', name)
+            elif 'This Result has not been declared yet!' in response.text:
+                print('No result')
+            elif 'Faculty_No or En_No is incorrect!' in response.text:
+                print('Wrong Faculty or Enrolment No.')
+            else:
+                print('Wrong input data or no result...')
+
+        except requests.exceptions.ConnectionError:
+            print('No Connection')
+
+
 def get_result(students):
-  for key in students.keys():
-    name = students[key]['name'] 
-    en_no = students[key]['en_no']
-    fac_no = students[key]['fac_no']
-    print('\nGetting result of ', fac_no, name)
-    for_student(fac_no, en_no, name)
-    #time.sleep(.5)
-  print('\n\nAll results saved')
-    
+    for key in students.keys():
+        name = students[key]['name']
+        en_no = students[key]['en_no']
+        fac_no = students[key]['fac_no']
+        print('\nGetting result of ', fac_no, name)
+        for_student(fac_no, en_no, name)
+        # time.sleep(.5)
+    print('\n\nAll results saved')
 
 
 def main(students):
-  menu = \
-'''
+    menu = \
+        '''
 
 
-1. Download Results. 
-2. Load Marks.
-3. Create result worksheet.
-4. Exit
+        1. Download Results.
+        2. Load Marks.
+        3. Create result worksheet.
+        4. Exit
 
-'''
-  
-  print(menu)
-  x = input()
-  
-  if x == '1' :
-    get_result(students)
-    input('Press any key to Continue...')
-  elif x == '2' :
-    utils.set_marks(students)
-    input('Press any key to Continue...')
-  elif x == '3' : 
-    print(utils.create_worksheet(name, students))
-    input('Press any key to Continue...')
-  elif x == '4' :
-    return
-  else:
-    print('Please choose valid option')
-    input('\n\n\nPress any key to Continue...')
-  main(students)
+        '''
 
-wrong=1
-while(wrong):
-  print('List of files in Input directory:')
-  enlist.list_input()
-  name = input('\n\nEnter the Excel file name: ').rstrip()
-  students = None
-  students = enlist.populate(name)
-  if students == None:
-    print('Error reading student database...\nHave you put Attendance file in Input/ folder and provided correct name?\n\nRetrying running script')
-  else:
-    wrong = 0
+    print(menu)
+    x = input()
+
+    if x == '1':
+        get_result(students)
+        input('Press any key to Continue...')
+    elif x == '2':
+        utils.set_marks(students)
+        input('Press any key to Continue...')
+    elif x == '3':
+        print(utils.create_worksheet(name, students))
+        input('Press any key to Continue...')
+    elif x == '4':
+        return
+    else:
+        print('Please choose valid option')
+        input('\n\n\nPress any key to Continue...')
     main(students)
+
+
+wrong = 1
+while (wrong):
+    print('List of files in Input directory:')
+    enlist.list_input()
+    name = input('\n\nEnter the Excel file name: ').rstrip()
+    students = None
+    students = enlist.populate(name)
+    if students == None:
+        print(
+            'Error reading student database...\nHave you put Attendance file in Input/ folder and provided correct name?\n\nRetrying running script')
+    else:
+        wrong = 0
+        main(students)
--- utils.py
+++ utils.py
@@ -3,130 +3,136 @@ import os
 import ast
 import xlsxwriter
 
+
 def path():
-  return os.path.dirname(os.path.realpath(__file__))
+    return os.path.dirname(os.path.realpath(__file__))
+
 
 def mkdirs():
-  dirs = [os.path.join(path(), 'iaj', 'Store'), os.path.join(path(), 'Input'), os.path.join(path(), 'Output')]
-  for dire in dirs :
-    if not os.path.exists(dire):
-      os.makedirs(dire)
+    dirs = [os.path.join(path(), 'iaj', 'Store'), os.path.join(path(), 'Input'), os.path.join(path(), 'Output')]
+    for dire in dirs:
+        if not os.path.exists(dire):
+            os.makedirs(dire)
+
 
 def cd(a=0):
-  if a == 0:
-    pathe = os.path.join(path(), 'iaj')
-  elif a == 1:
-    pathe = os.path.join(path(), 'Output')
-  elif a == 2:
-    pathe = os.path.join(path(),'iaj', 'Store')
-  os.chdir(pathe)
-  
+    if a == 0:
+        pathe = os.path.join(path(), 'iaj')
+    elif a == 1:
+        pathe = os.path.join(path(), 'Output')
+    elif a == 2:
+        pathe = os.path.join(path(), 'iaj', 'Store')
+    os.chdir(pathe)
 
 
 def save(students):
-  cd()
-  with open('results.db', 'w+') as fi:
-      print(students, file=fi)
+    cd()
+    with open('results.db', 'w+') as fi:
+        print(students, file=fi)
+
 
 def load_all(students):
-  for key in students.keys():
+    for key in students.keys():
+        name = students[key]['name']
+        fac_no = students[key]['fac_no']
+        r_no = fac_no[5:8]
+        file_name = r_no + ' - ' + name + ' (' + fac_no + ')' + '.html'
+        cd(2)
+        try:
+            with open(file_name, 'r+') as fi:
+                page = fi.read()
+                soup = BeautifulSoup(page)
+        except IOError:
+            print('HTML file not found. Download files by option 1 first...')
+            return
+        try:
+            spi = soup('table')[1].findAll('td')[4].string.strip()
+            cpi = soup('table')[1].findAll('td')[5].string.strip()
+        except IndexError:
+            spi = 0
+            cpi = 0
+        students[key]['spi'] = spi
+        students[key]['cpi'] = cpi
+        print('Loaded:', students[key]['name'])
+    save(students)
+    return students
+
+
+def load(students, key):
     name = students[key]['name']
     fac_no = students[key]['fac_no']
     r_no = fac_no[5:8]
-    file_name = r_no + ' - ' + name + ' (' + fac_no + ')' +'.html'
+    file_name = r_no + ' - ' + name + ' (' + fac_no + ')' + '.html'
     cd(2)
-    try:
-      with open(file_name, 'r+') as fi:
+    with open(file_name, 'r+') as fi:
         page = fi.read()
         soup = BeautifulSoup(page)
-    except IOError:
-      print('HTML file not found. Download files by option 1 first...') 
-      return
     try:
-      spi = soup('table')[1].findAll('td')[4].string.strip()
-      cpi = soup('table')[1].findAll('td')[5].string.strip()
+        spi = soup('table')[2].findAll('tr')[1].findAll('th')[4].string.strip()
+        cpi = soup('table')[2].findAll('tr')[1].findAll('th')[5].string.strip()
     except IndexError:
-      spi = 0
-      cpi = 0
-    students[key]['spi'] = spi
-    students[key]['cpi'] = cpi
-    print('Loaded:', students[key]['name'])
-  save(students)
-  return students
+        spi = 0
+        cpi = 0
+    return {'cpi': cpi, 'spi': spi}
 
-def load(students, key):
-  name = students[key]['name']
-  fac_no = students[key]['fac_no']
-  r_no = fac_no[5:8]
-  file_name = r_no + ' - ' + name + ' (' + fac_no + ')' +'.html'
-  cd(2)
-  with open(file_name, 'r+') as fi:
-    page = fi.read()
-    soup = BeautifulSoup(page)
-  try:
-    spi = soup('table')[2].findAll('tr')[1].findAll('th')[4].string.strip()
-    cpi = soup('table')[2].findAll('tr')[1].findAll('th')[5].string.strip()
-  except IndexError:
-    spi = 0
-    cpi = 0
-  return {'cpi':cpi, 'spi' : spi}
-    
+
 def set_marks(students):
-  cd()
-  if os.path.isfile('results.db'):
-    with open('results.db', 'r+') as fi:
-      data = fi.read()
-      res = ast.literal_eval(data)
-    if students[1]['name']==res[1]['name']:
-      pass
-    else :
-      print('Database mismatch. Did you download result first?')
-      return
-    for key in students.keys():
-      try:
-        students[key]['cpi'] = res[key]['cpi']
-        students[key]['spi'] = res[key]['spi']
-      except KeyError :
-        students[key]['cpi'] = load(students, key)['cpi']
-        students[key]['spi'] = load(students, key)['spi']
-    save(students)
-    print('Marks loaded')
-    return students
-  else:
-    load_all(students)
+    cd()
+    if os.path.isfile('results.db'):
+        with open('results.db', 'r+') as fi:
+            data = fi.read()
+            res = ast.literal_eval(data)
+        if students[1]['name'] == res[1]['name']:
+            pass
+        else:
+            print('Database mismatch. Did you download result first?')
+            return
+        for key in students.keys():
+            try:
+                students[key]['cpi'] = res[key]['cpi']
+                students[key]['spi'] = res[key]['spi']
+            except KeyError:
+                students[key]['cpi'] = load(students, key)['cpi']
+                students[key]['spi'] = load(students, key)['spi']
+        save(students)
+        print('Marks loaded')
+        return students
+    else:
+        load_all(students)
+
 
 def create_worksheet(name, students):
-  pathe = os.path.join(path(), 'Output')
-  if not os.path.exists(pathe):
-    os.makedirs(pathe)
-  cd(1)
-  if os.path.isfile(name):
-    over = input('Worksheet already exists. Do you want to over write?\nY for yes, any key for no... \n')
-    if over == 'Y' or over == 'y':
-      print('Over writing file') 
-    else:
-      return 'Skipping writing file...\n Check in Output/ directory'
-  try :
-    students[1]['cpi']
-  except KeyError as err :
-    return 'Wrong input file. Run 2. Load marks first...'
-  data = xlsxwriter.Workbook(name)
-  data_sheet = data.add_worksheet() 
-  
-  head = ['S. No.', 'Faculty No.', 'Enrolment No.', 'Name', 'CPI', 'SPI']
-  col=0
-  for cell in head:
-    data_sheet.write(0,col, cell)
-    col+=1
+    pathe = os.path.join(path(), 'Output')
+    if not os.path.exists(pathe):
+        os.makedirs(pathe)
+    cd(1)
+    if os.path.isfile(name):
+        over = input('Worksheet already exists. Do you want to over write?\nY for yes, any key for no... \n')
+        if over == 'Y' or over == 'y':
+            print('Over writing file')
+        else:
+            return 'Skipping writing file...\n Check in Output/ directory'
+    try:
+        students[1]['cpi']
+    except KeyError as err:
+        return 'Wrong input file. Run 2. Load marks first...'
+    data = xlsxwriter.Workbook(name)
+    data_sheet = data.add_worksheet()
 
-  for key in students.keys():
-    col=0
-    student = [key, students[key]['fac_no'], students[key]['en_no'], students[key]['name'], students[key]['cpi'], students[key]['spi']]
-    for each in student:
-      data_sheet.write(key, col, each)
-      col+=1
-      	
-  data.close()
-  success = 'Successfully created '+name+' in Output/ directory'
-  return success
-      
+    head = ['S. No.', 'Faculty No.', 'Enrolment No.', 'Name', 'CPI', 'SPI']
+    col = 0
+    for cell in head:
+        data_sheet.write(0, col, cell)
+        col += 1
+
+    for key in students.keys():
+        col = 0
+        student = [key, students[key]['fac_no'], students[key]['en_no'], students[key]['name'], students[key]['cpi'],
+                   students[key]['spi']]
+        for each in student:
+            data_sheet.write(key, col, each)
+            col += 1
+
+    data.close()
+    success = 'Successfully created ' + name + ' in Output/ directory'
+    return success
