292fd0e9717cd48bd6ed4476b501cbafeb6a5c2a
Areeb Jamal <jamal.areeb@gmail.com>
2017-06-29 06:43:59 +0530

    feat: Add threading to download results in parallel

--- result.py
+++ result.py
@@ -1,7 +1,10 @@
 import os
 import requests
+import sys
 from bs4 import BeautifulSoup
 from collections import OrderedDict
+from Queue import Queue
+from threading import Thread
 
 
 def get_file_name(base_dir, student):
@@ -11,33 +14,56 @@ def get_file_name(base_dir, student):
 class Downloader:
     __base_url_format = "http://ctengg.amu.ac.in/web/table_resultnew.php?fac=%s&en=%s&prog=btech"
     __store_path = 'store'
+    __concurrent = 200
 
     def __init__(self, student_list):
         if not os.path.exists(self.__store_path):
             os.mkdir(self.__store_path)
         self.students = student_list
+        self.q = Queue(self.__concurrent * 2)
+        for i in xrange(self.__concurrent):
+            thread = Thread(target = self.consume)
+            thread.daemon = True
+            thread.start()
+
+    def consume(self):
+        while True:
+            student = self.q.get()
+            if not self.process(student):
+                self.q.put(student)
+            self.q.task_done()
+
+    def process(self, student):
+        file_path = get_file_name(self.__store_path, student)
+        fac_no = student['fac_no']
+        name = student['name']
+        if os.path.isfile(file_path):
+            print "Result for %s - %s already exists. Skipping..." % (fac_no, name)
+        else:
+            en_no = student['en_no']
+            response = requests.get(
+                self.__base_url_format % (fac_no, en_no))
+
+            content = response.text
+            if response and 'CPI' in content:
+                out = open(file_path, 'w+')
+                out.write(content)
+                out.close()
+                print 'Result Downloaded %s' % file_path
+            elif response and 'Student record not found' in content:
+                print 'Either result is unavailable or error in credentials %s %s %s' % (fac_no, en_no, student['name'])
+            else:
+                return False
+        return True
 
     def download(self):
-        for student in self.students:
-            file_path = get_file_name(self.__store_path, student)
-            fac_no = student['fac_no']
-            name = student['name']
-            if os.path.isfile(file_path):
-                print "Result for %s - %s already exists. Skipping..." % (fac_no, name)
-            else:
-                print "Downloading result for %s - %s..." % (fac_no, name)
-                en_no = student['en_no']
-                response = requests.get(
-                    self.__base_url_format % (fac_no, en_no))
-
-                content = response.text
-                if response and 'CPI' in content:
-                    out = open(file_path, 'w+')
-                    out.write(content)
-                    out.close()
-                    print 'Result Downloaded'
-                else:
-                    print 'Either result is unavailable or error in credentials'
+        print 'Started downloading...'
+        try:
+            for student in self.students:
+              self.q.put(student)
+            self.q.join()
+        except KeyboardInterrupt:
+            sys.exit(1)
 
     def get_store_dir(self):
         return self.__store_path
